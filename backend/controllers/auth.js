6063b5a623cf2624a7b5cb040b647e633e5d4a1e
Vitor Gomes <vitorgomes605@gmail.com>
2023-10-23 20:12:03 -0300

    Schema and Register

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,9 +1,10 @@
+const User = require('../models/User')
 
 
 
 const register = async (req,res) => {
-    res.send('Register')
- 
+   const user = await User.create(req.body)
+    res.send({user})
  }
 
  const login = async (req,res) => {
---
+++ backend/models/User.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
+const Schema = mongoose.Schema
+
+const AuthSchema = new Schema({
+    name: {
+        type:String,
+        required:[true, 'Please provide a name'],
+        minLength:3,
+        maxLength:50,
+
+    },
+    email: {
+        type: String,
+        required: [true, 'Please provide email'],
+        match: [
+          /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+          'Please provide a valid email',
+        ],
+        unique: true,
+      },
+      password: {
+        type:String,
+        required:[true, 'Please provide a password'],
+        minLength:6,
+       
+
+    },
+}, {timestamps: true})
+
+
+AuthSchema.pre('save', async function (next) {
+  //encrypt password
+  const salt = await bcrypt.genSalt(10)
+  const hashedPassword = await bcrypt.hash(this.password, salt)
+  this.password = hashedPassword
+  next();
+});
+
+module.exports = mongoose.model('User', AuthSchema)
