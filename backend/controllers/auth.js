cc30610d5e6c27b4e5eac010f9592e256d413576
Vitor Gomes <vitorgomes605@gmail.com>
2023-10-25 14:45:39 -0300

    Login

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,20 +1,72 @@
-const { StatusCodes } = require('http-status-codes')
+const {
+	StatusCodes
+} = require('http-status-codes')
 const User = require('../models/User')
+const bcrypt = require('bcryptjs')
+const {
+	BadRequestError,
+	UnauthenticatedError
+} = require('../errors')
 
 
 
-const register = async (req,res) => {
-   const user = await User.create(req.body)
-   const token = user.createJWT()
-    res.status(StatusCodes.CREATED).json({user:{name: user.name}, token})
- }
+const register = async (req, res) => {
+	const user = await User.create(req.body)
+	const token = user.createJWT()
+	res.status(StatusCodes.CREATED).json({
+		user: {
+			name: user.name
+		},
+		token
+	})
+}
 
- const login = async (req,res) => {
-    res.send('login')
- 
- }
+const login = async (req, res) => {
+	const {
+		email,
+		password
+	} = req.body
 
- module.exports = {
-    register,
-    login
- }
+	if (!email) {
+		throw new BadRequestError('Must provide Email ')
+	}
+	if (!password) {
+		throw new BadRequestError('Must provide Password')
+	}
+
+	const user = await User.findOne({
+		email
+	})
+
+	if (!user) {
+		throw new BadRequestError('Invalid Email')
+	}
+
+	const comparePassword = async (passwordToCompare) => {
+		const match = await bcrypt.compare(passwordToCompare, user.password)
+		return match
+	}
+
+
+	const isValid = await comparePassword(password)
+
+	if (!isValid) {
+		throw new BadRequestError('Senha Incorreta')
+	}
+
+
+
+	const token = user.createJWT()
+	res.status(StatusCodes.CREATED).json({
+		user: {
+			name: user.name
+		},
+		token
+	})
+
+}
+
+module.exports = {
+	register,
+	login
+}
